6070580772cb94acc3b60546c4365b5b8635da8a
brahianpdev <zk.spywn@gmail.com>
2022-04-13 00:59:07 -0300

    Fixed: Route indentation

--- src/routes/posts.routes.ts
+++ src/routes/posts.routes.ts
@@ -2,19 +2,44 @@ import { Router } from "express";
 const routes = Router();
 
 import postsController from "../controllers/posts.controller";
-import authMiddleware from '../middlewares/authentication.middleware';
-import validationMiddleware from '../middlewares/validation.middleware';
-import { CreatePostDTO } from '../types/dtos/create-post.dto';
-import { UpdatePostDTO } from '../types/dtos/update-post.dto'
-import { ChangeStateDTO } from '../types/dtos/change-state.dto';
+import authMiddleware from "../middlewares/authentication.middleware";
+import validationMiddleware from "../middlewares/validation.middleware";
+import { CreatePostDTO } from "../types/dtos/create-post.dto";
+import { UpdatePostDTO } from "../types/dtos/update-post.dto";
+import { ChangeStateDTO } from "../types/dtos/change-state.dto";
 
 routes
   .get("/", postsController.findAll)
   .get("/:id", postsController.findById)
-  .post("/create/", authMiddleware, validationMiddleware(CreatePostDTO, true), postsController.create)
-  .post("/remove/:id", authMiddleware, validationMiddleware(ChangeStateDTO), postsController.remove)
-  .post("/publish/:id", authMiddleware, validationMiddleware(ChangeStateDTO), postsController.publish) 
-  .put("/update/:id", authMiddleware, validationMiddleware(UpdatePostDTO), postsController.update)
+
+  .post(
+    "/create/",
+    authMiddleware,
+    validationMiddleware(CreatePostDTO, true),
+    postsController.create
+  )
+
+  .post(
+    "/remove/:id",
+    authMiddleware,
+    validationMiddleware(ChangeStateDTO),
+    postsController.remove
+  )
+
+  .post(
+    "/publish/:id",
+    authMiddleware,
+    validationMiddleware(ChangeStateDTO),
+    postsController.publish
+  )
+
+  .put(
+    "/update/:id",
+    authMiddleware,
+    validationMiddleware(UpdatePostDTO),
+    postsController.update
+  )
+  
   .delete("/delete/:id", authMiddleware, postsController.delete);
 
 export default routes;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,14 +3,21 @@ const routes = Router();
 
 import usersController from "../controllers/users.controller";
 import authMiddleware from "../middlewares/authentication.middleware";
-import validationMiddleware from '../middlewares/validation.middleware';
+import validationMiddleware from "../middlewares/validation.middleware";
 import { UpdateUserDTO } from "../types/dtos/update-user.dto";
 
 routes
   .get("/", usersController.getAllUsers)
   .get("/email/:email", authMiddleware, usersController.findUserByEmail)
   .get("/:id", usersController.findUserById)
-  .put("/update/:id", authMiddleware, validationMiddleware(UpdateUserDTO), usersController.updateUser)
+
+  .put(
+    "/update/:id",
+    authMiddleware,
+    validationMiddleware(UpdateUserDTO),
+    usersController.updateUser
+  )
+
   .delete("/delete/:id", authMiddleware, usersController.deleteUser);
 
 export default routes;
