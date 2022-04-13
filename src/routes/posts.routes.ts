82ffb70a73ac2da8a7577d4bac643a538039cf08
brahianpdev <zk.spywn@gmail.com>
2022-04-13 00:54:48 -0300

    Fixed: Endpoints of users routes

--- src/routes/posts.routes.ts
+++ src/routes/posts.routes.ts
@@ -6,12 +6,15 @@ import authMiddleware from '../middlewares/authentication.middleware';
 import validationMiddleware from '../middlewares/validation.middleware';
 import { CreatePostDTO } from '../types/dtos/create-post.dto';
 import { UpdatePostDTO } from '../types/dtos/update-post.dto'
+import { ChangeStateDTO } from '../types/dtos/change-state.dto';
 
 routes
   .get("/", postsController.findAll)
   .get("/:id", postsController.findById)
-  .post("/", authMiddleware, validationMiddleware(CreatePostDTO, true), postsController.create)
-  .put("/:id", authMiddleware, validationMiddleware(UpdatePostDTO), postsController.update)
-  .delete("/:id", authMiddleware, postsController.delete);
+  .post("/create/", authMiddleware, validationMiddleware(CreatePostDTO, true), postsController.create)
+  .post("/remove/:id", authMiddleware, validationMiddleware(ChangeStateDTO), postsController.remove)
+  .post("/publish/:id", authMiddleware, validationMiddleware(ChangeStateDTO), postsController.publish) 
+  .put("/update/:id", authMiddleware, validationMiddleware(UpdatePostDTO), postsController.update)
+  .delete("/delete/:id", authMiddleware, postsController.delete);
 
 export default routes;
