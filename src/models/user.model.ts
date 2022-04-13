8c96ce2ef67f64898a3a8c1eabd64f2ce087ceef
brahianpdev <zk.spywn@gmail.com>
2022-04-13 19:46:06 -0300

    Feat: New props on User Schema

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -2,6 +2,21 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const User = new Schema({
+  nickname: {
+    type: String,
+    required: true, 
+  },
+  avatar: {
+    type: String,
+    required: false,
+    default: "https://media.redadn.es/imagenes/animanga-0_342458_pn2.jpg",
+  },
+  role: {
+    type: String,
+    required: false,
+    enum: ["USER", "ADMIN"],
+    default: "USER",
+  },
   email: {
     type: String,
     required: true,
