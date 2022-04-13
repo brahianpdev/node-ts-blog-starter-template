fd96d122335846703fdb72b0fd8b673ae7e80dca
brahianpdev <zk.spywn@gmail.com>
2022-04-13 19:45:43 -0300

    Fixed: Nodemailer enviroments from

--- src/services/nodemailer.service.ts
+++ src/services/nodemailer.service.ts
@@ -1,10 +1,11 @@
 import { transporter } from "../configuration/nodemailerConfig";
+import enviromentConfig from '../configuration/enviromentConfig';
 
 export class NodemailerService {
   async sendEmail(email: string, subject: string, html: string) {
     try {
       await transporter.sendMail({
-        from: process.env.USER,
+        from: enviromentConfig.nodemailer.user,
         to: email,
         subject: subject,
         text: html,
