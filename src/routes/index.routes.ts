7895242ab755527dbf0d2a6c14d94b20bc1db3d9
brahianpdev <zk.spywn@gmail.com>
2022-04-14 15:44:00 -0300

    Add categories and comments routes to index routes

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -4,9 +4,13 @@ const routes = Router();
 import userRoutes from "./users.routes";
 import authRoutes from "./authentication.routes";
 import postsRoutes from "./posts.routes";
+import categoriesRoutes from "./categories.routes";
+import commentsRoutes from "./comments.routes";
 
 routes.use("/auth",  authRoutes);
 routes.use("/users", userRoutes);
 routes.use("/posts", postsRoutes);
+routes.use("/categories", categoriesRoutes);
+routes.use("/comments", commentsRoutes);
 
 export default routes;
